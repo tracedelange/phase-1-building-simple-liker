cd0409b1ed33dc9146990bd1e7415153e7d6b300
Trace <tracedelange@me.com>
2021-06-17 12:05:34 -0500

    commit tree 5ce95e8816e679390633b9799e7f6c5bc7185f93

--- main.js
+++ main.js
@@ -3,9 +3,54 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// DOM object definitions
+const likeListItems = document.getElementsByClassName('like-glyph')
+const errorModal = document.getElementById('modal')
 
 
+const init = () => {
+  //funciton to be called on DOM load complete
 
+  likeButtonInit()
+}
+
+const likeButtonInit = () => {
+  for (let item of likeListItems){
+    item.addEventListener('click', function(event){
+      likeButtonClicked(event)
+    })
+  }
+}
+
+const successfulCom = (target) => {
+
+  if ( target.innerText === EMPTY_HEART ){
+    target.innerText = FULL_HEART
+    target.className = 'activated-heart like-glyph'
+  } else if ( target.innerText === FULL_HEART ){    
+    target.innerText = EMPTY_HEART
+    target.className = 'like-glyph'
+  }
+}
+
+const failedCom = () => {
+
+  errorModal.className = ''
+  setTimeout(function(){ errorModal.className = 'hidden' }, 3000);
+}
+
+const likeButtonClicked = (event) => {
+  let target = event.target
+
+  mimicServerCall()
+  .then((response)=> successfulCom(target))
+  .catch((error) => failedCom())
+
+
+}
+
+
+document.addEventListener('DOMContentLoaded', init)
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
